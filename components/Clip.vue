1f84f8fba857492b3dbd7952db50a0d2a24d7bc5
ihunter <ijh.hunter@gmail.com>
2020-08-16 23:54:30 -0400

    add rel="noopener noreferrer nofollow" to external links

--- components/Clip.vue
+++ components/Clip.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card tag="article">
-    <a :href="url" target="_blank">
+    <a :href="url" target="_blank" rel="noopener noreferrer nofollow">
       <v-img
         min-height="200"
         :src="thumbnailUrl"
