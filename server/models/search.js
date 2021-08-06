9e90a934d874c49d8953d395b960a225e1c0bd15
ihunter <ijh.hunter@gmail.com>
2021-08-06 00:48:11 -0400

    virtual foreign fields population for games and broadcaster for search logs

--- server/models/search.js
+++ server/models/search.js
@@ -3,14 +3,12 @@ const Schema = mongoose.Schema
 
 const searchSchema = new Schema({
   broadcaster_id: {
-    type: [String]
+    type: [{ type: String, ref: 'Broadcaster' }]
   },
   creator_name: {
     type: String
   },
-  game_id: {
-    type: [String]
-  },
+  game_id: [{ type: String, ref: 'Game' }],
   title: {
     type: String
   },
@@ -20,6 +18,18 @@ const searchSchema = new Schema({
   endDate: {
     type: Date
   }
-}, { timestamps: true })
+}, { toJSON: { virtuals: true }, timestamps: true })
+
+searchSchema.virtual('game', {
+  ref: 'Game',
+  localField: 'game_id',
+  foreignField: 'id'
+})
+
+searchSchema.virtual('broadcaster', {
+  ref: 'Broadcaster',
+  localField: 'broadcaster_id',
+  foreignField: 'id'
+})
 
 module.exports.Search = mongoose.model('Search', searchSchema)
