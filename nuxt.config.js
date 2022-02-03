10bb49ac65aad0400484e68dfbddf8551a50a29b
ihunter <ijh.hunter@gmail.com>
2022-02-03 02:12:47 -0500

    vue-gtag

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,11 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    {
+      src: '~/plugins/vue-gtag'
+    }
+  ],
   /*
    ** Nuxt.js dev-modules
    */
