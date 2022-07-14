0b77e6c3e77af8fc881417facff9fb92777bdb78
ihunter <ijh.hunter@gmail.com>
2022-07-14 00:21:47 -0400

    remove mongodb uri

--- server/utils/pruneClips.js
+++ server/utils/pruneClips.js
@@ -1,9 +1,11 @@
+
+require('dotenv').config()
 const mongoose = require('mongoose')
 const { Clip } = require('../models')
 
 async function prune() {
   try {
-    await mongoose.connect('mongodb+srv://Hunter:sF9eb40L4gt24nHe@cluster0-nd0zz.mongodb.net/twitch-clip-search?retryWrites=true&w=majority&readPreference=primaryPreferred')
+    await mongoose.connect(process.env.MONGODB_URI)
   } catch (error) {
     console.error(error)
   }
