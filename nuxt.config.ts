b71372403e9624940fb61052a60b66c495ac4ba0
ihunter <ijh.hunter@gmail.com>
2023-06-17 01:44:05 -0400

    update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/devtools',
     '@nuxtjs/tailwindcss',
     'nuxt-graphql-server',
   ],
+
   graphqlServer: {
     url: '/api/graphql',
     schema: './server/**/*.graphql',
   },
+
   runtimeConfig: {
     mongoURI: process.env.NUXT_MONGO_URI,
+  },
+
+  devtools: {
+    enabled: true
   }
 })
