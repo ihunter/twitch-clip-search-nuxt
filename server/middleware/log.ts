4bf2516640840163c22f41d2b3112213d24d8ba5
ihunter <ijh.hunter@gmail.com>
2024-09-09 18:51:53 -0400

    .

--- server/middleware/log.ts
+++ server/middleware/log.ts
@@ -3,5 +3,5 @@ export default defineEventHandler((event) => {
   const ipAddress = event.node.req.headers['x-forwarded-for'] || event.node.req.socket.remoteAddress
 
   console.log('userAgent:', userAgent)
-  console.log('ip:', ipAddress)
+  console.log('ip:', ipAddress, typeof ipAddress)
 })
