f66d0e60661e92d53e042fd4cff79b0d2595c0ef
ihunter <ijh.hunter@gmail.com>
2024-09-10 02:16:17 -0400

    filter request in middleware

--- server/middleware/log.ts
+++ server/middleware/log.ts
@@ -1,14 +1,21 @@
 import { clipsQuery } from '~/server/utils/queryParser'
 
 export default defineEventHandler((event) => {
+  const { method, url } = event.node.req
+
+  if (!(method === 'GET' && url?.startsWith('/api/clips')))
+    return
+
   const userAgent = getRequestHeader(event, 'user-agent')
   const ipAddresses = event.node.req.headers['x-forwarded-for'] || event.node.req.socket.remoteAddress
 
   const ipAddress = Array.isArray(ipAddresses) ? ipAddresses[0] : ipAddresses?.split(',')[0]
 
-  const { query } = clipsQuery(event)
+  const { query, page } = clipsQuery(event)
 
-  console.log('userAgent:', userAgent)
-  console.log('ip:', ipAddress)
-  console.log('query:', query)
+  if (page === '1') {
+    console.log('userAgent:', userAgent)
+    console.log('ip:', ipAddress)
+    console.log('query:', query)
+  }
 })
